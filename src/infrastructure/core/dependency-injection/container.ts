3d4564c5575df20730595a5787ae7d17c6ae6a30
F Javier López Herrera <fjavierlh@gmail.com>
2022-05-09 19:09:51 +0200

    chore: Created container di bootstrap function

---
+++ src/infrastructure/core/dependency-injection/container.ts
@@ -0,0 +1,18 @@
+import Container from 'typedi';
+import FilesRepository from '../../File/FilesRepository';
+import ImagesRepository from '../../Image/ImagesRepository';
+import ImageResizerHttp from '../../ImageResizer/ImageResizerHttp';
+import FecthHttpClient from '../../Shared/HttpClient/FetchHttpClient';
+import TasksRepository from '../../Task/TasksRepository';
+
+const bootstrapContainerDI = () => {
+  Container.set('TasksRepository', new TasksRepository());
+  Container.set('ImagesRepository', new ImagesRepository());
+  Container.set('FilesRepository', new FilesRepository());
+  Container.set(
+    'ImageResizerHttp',
+    new ImageResizerHttp(new FecthHttpClient('http://localhost:8080'))
+  );
+};
+
+export default bootstrapContainerDI;
