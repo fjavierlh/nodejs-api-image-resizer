2c31372e31ae849b34a2e0169624922e3ce4d2fa
F Javier López Herrera <fjavierlh@gmail.com>
2022-05-10 11:49:37 +0200

    chore: Created register and register routes functions

---
+++ src/infrastructure/core/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import glob from 'glob';
+
+export function registerRoutes(router: Router) {
+  const routes = glob.sync(process.cwd() + '/dist/**/**/*.route.*');
+  routes.map((route) => register(route, router));
+}
+
+function register(routePath: string, router: Router) {
+  const route = require(routePath);
+  route.register(router);
+}
