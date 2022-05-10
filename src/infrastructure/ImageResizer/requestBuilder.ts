f145004185bada4fcaf01412c43ce597776890e2
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2022-05-10 18:28:33 +0200

    style: deleted missing comments

--- src/infrastructure/ImageResizer/requestBuilder.ts
+++ src/infrastructure/ImageResizer/requestBuilder.ts
@@ -1,21 +1,6 @@
 import FormData from 'form-data';
 import fs from 'fs-extra';
 
-// const buildResizeImageRequestFormData = (file: string) => {
-//   const stats = fs.statSync(file);
-//   const fileSizeInBytes = stats.size;
-
-//   const bufferContent = fs.readFileSync(file);
-
-//   return {
-//     headers: {
-//       'Content-Type': 'multipart/form-data',
-//       'Content-Length': fileSizeInBytes
-//     },
-//     body: bufferContent
-//   };
-// };
-
 const buildResizeImageRequestFormData = (file: string) => {
   const formData = new FormData();
   formData.append('image', fs.createReadStream(file));
