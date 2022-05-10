2d701717bf36edf438dcbb34b55999827c9b4732
F Javier López Herrera <fjavierlh@gmail.com>
2022-05-10 11:42:41 +0200

    chore: created isUuid and timestamp utils

---
+++ src/domain/Shared/utils/isUuid.ts
@@ -0,0 +1,7 @@
+const isUuid = (value: string): boolean => {
+  const regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return regex.test(value);
+};
+
+export { isUuid };
---
+++ src/domain/Shared/utils/timestamp.ts
@@ -0,0 +1,3 @@
+const timestamp = () => new Date().getTime().toString();
+
+export { timestamp };
